229bbc18ad1e523c0bb5102fa64a846af338dd95
Ian Maddocks <37927672+ianmaddocks@users.noreply.github.com>
2022-04-25 15:57:21 +0100

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -7,7 +7,7 @@ terraform apply -auto-approve
 
 printf "\nWaiting for the echo web server service... \n"
 kubectl apply -f app-services.yaml
-sleep 10
+sleep 15
 
 printf "\nYou should see 'foo' as a reponse below (if you do the ingress is working):\n"
 curl http://localhost/bar
