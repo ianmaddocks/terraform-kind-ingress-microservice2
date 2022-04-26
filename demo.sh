277b4adfbb1572cc992fd74c35bda3e5ae1ddd4f
ianmaddocks <ian@maddocks.name>
2022-04-26 10:06:49 +0100

    split apply.yaml (the services) into sepurate yaml files per service

--- demo.sh
+++ demo.sh
@@ -6,8 +6,10 @@ terraform plan
 terraform apply -auto-approve
 
 printf "\nWaiting for the echo web server service... \n"
-kubectl apply -f app-services.yaml
-sleep 10
+#kubectl apply -f app-services.yaml
+kubectl apply -f app-bar.yaml
+kubectl apply -f app-microservice2.yaml
+sleep 15
 
 printf "\nYou should see 'foo' as a reponse below (if you do the ingress is working):\n"
 curl http://localhost/bar
